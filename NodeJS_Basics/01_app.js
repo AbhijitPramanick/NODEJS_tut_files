609f38e9f523d3762daecbca454850f0a0ffa457
AbhijitPramanick <amanik1995@gmail.com>
2023-09-25 16:21:45 +0530

    Read input and Write output

---
+++ NodeJS_Basics/01_app.js
@@ -0,0 +1,21 @@
+// LECTURE 4 : READING INPUT AND WRITING OUTPUT
+// https://youtu.be/VP55JN3y6uI?si=cf542niEDIf26joE
+
+// console.log("Hello world, this is Node JS");
+// console.log("Hello world, this is my first Node JS App");
+
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// rl.question("Please enter your name: ", (firstName) => {
+//   console.log("The entered name is " + firstName);
+//   rl.close();
+// });
+// rl.on("close", () => {
+//   console.log("Interface closed");
+//   process.exit(0);
+// });
+
+// --------------------------------------
